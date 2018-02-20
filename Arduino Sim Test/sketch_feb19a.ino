f30e60a9112175de1b066acd73426d741bdf58e7
ali-taalab <36545889+ali-taalab@users.noreply.github.com>
2018-02-20 14:43:17 +0200

    Add files via upload

---
+++ Arduino Sim Test/sketch_feb19a.ino
@@ -0,0 +1,21 @@
+int inputPins [] = {10,11,12,13};
+int ledPins [] = {0,1,2,3};
+
+void setup() {
+  // put your setup code here, to run once:
+  for(int i = 0; i<4; i++)
+  {
+    pinMode(ledPins[i] , OUTPUT);
+    pinMode(inputPins[i] , INPUT);
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for(int i =0; i<4; i++)
+  {
+    int value = digitalRead(inputPins[i]);
+    digitalWrite (ledPins[i] , value);
+  }
+
+}
