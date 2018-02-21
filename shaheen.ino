39139f3e685215941e60e0a937e80c39139b1333
Ali-shaheen <36548286+Ali-shaheen@users.noreply.github.com>
2018-02-21 22:55:08 +0200

    Add files via upload

---
+++ shaheen.ino
@@ -0,0 +1,13 @@
+int ledPin=13;
+int buttonPin=7;
+void setup()
+{
+pinMode(ledPin, OUTPUT);
+ pinMode(buttonPin, INPUT);
+ digitalWrite(buttonPin,HIGH);
+}
+void loop()
+{
+  int value=digitalRead(buttonPin);
+  digitalWrite(ledPin, value);
+}
